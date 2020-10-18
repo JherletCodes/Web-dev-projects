1a295395437c04e057573c42e2bd34c8c7563cd5
John Jherlet Hollero <53713996+jherlet21@users.noreply.github.com>
2020-10-18 15:20:06 +0800

    Add files via upload

---
+++ Change-Colors/function.js
@@ -0,0 +1,20 @@
+function changeColor(){
+    
+var divElement1=
+document.getElementById("yellow-color");
+var divElement2=
+document.getElementById("blue-color");
+divElement1.className="blue-back";
+divElement2.className="yellow-back";
+
+}
+function ReturnColor(){
+    
+    var divElement1=
+    document.getElementById("yellow-color");
+    var divElement2=
+    document.getElementById("blue-color");
+    divElement1.className="yellow-back";
+    divElement2.className="blue-back";
+    
+    }
